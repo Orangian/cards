aad2cd21a8189594e93b7dd50e5e07dd7a546d40
Eile <tysayahi@gmail.com>
2020-10-02 08:18:47 -0400

    Auto stash before merge of "master" and "origin/master"

--- build.sh
+++ build.sh
@@ -5,25 +5,33 @@ umask 0022 # Correct file permissions
 
 pacman -Syu archiso --noconfirm
 cp -r /usr/share/archiso/configs/releng/ ${PROFILE}
-cp -rf ./custom ${PROFILE}
+cp -rf ./cards/. ${PROFILE}
 
 tee -a ${PROFILE}/packages.x86_64 > /dev/null <<EOT
 xorg
+xorg-drivers
+xorg-server
+xorg-xinit
+xf86-input-libinput
+xterm
+lightdm
 mesa
-xf86-video-amdgpu
-xf86-video-intel
-nvidia
-nvidia-utils
-pantheon
-gnome-disk-utility
+i3-gaps
+switchboard-plug-desktop
+switchboard-plug-locale
+switchboard-plug-security-privacy
+virtualbox-guest-utils
 EOT
 
 echo -e "packages.x86_64:\n---"
 echo "$(<${PROFILE}/packages.x86_64)"
 echo "---"
 
+rm -f ${PROFILE}/airootfs/etc/systemd/system/getty@tty1.service.d/autologin.conf
+ln -s /lib/systemd/system/lightdm.service ${PROFILE}/airootfs/etc/systemd/system/display-manager.service
+
 mkdir ./out
 mkdir /tmp/archiso-tmp
 mkarchiso -v -w /tmp/archiso-tmp ${PROFILE}
 rm -rf /tmp/archiso-tmp
-mv ./out/archlinux-*-x86_64.iso ~/cards.iso
+mv ./out/cards-*.*.*-x86_64.iso ~
