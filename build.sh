a0c5f632febf4f0b781c02721769f63311ad7a97
Ty Ethan Sayahi <tysayahi@gmail.com>
2020-10-05 08:23:47 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,96 +1,108 @@
 #!/bin/bash
-export PROFILE=~/cards-profile
+export HOME=~
+export PROFILE=${HOME}/cards-profile
+export LOCAL_REPO=${HOME}/local-repo
 set +h
 umask 0022 # Correct file permissions
+systemd-machine-id-setup # Prevents errors when building AUR packages
 
-pacman -Syu archiso --noconfirm
-cp -r /usr/share/archiso/configs/releng/ ${PROFILE}
-rm ${PROFILE}/packages.x86_64
-cp -rf ./cards/. ${PROFILE}
+pacman -Syu archiso git base-devel jq expac diffstat pacutils wget devtools libxslt cmake \
+intltool gtk-doc gobject-introspection gnome-common polkit dbus-glib --noconfirm --noprogressbar # Install packages we'll need to build
 
-tee -a ${PROFILE}/packages.x86_64 > /dev/null <<EOT
-virtualbox-guest-utils
-amd-ucode
-arch-install-scripts
-b43-fwcutter
-base
-bind-tools
-broadcom-wl
-btrfs-progs
-ddrescue
-dhclient
-dhcpcd
-diffutils
-dmraid
-dnsmasq
-dosfstools
-edk2-shell
-efibootmgr
-ethtool
-exfatprogs
-f2fs-tools
-fsarchiver
-gnu-netcat
-gpm
-gptfdisk
-grml-zsh-config
-haveged
-hdparm
-intel-ucode
-ipw2100-fw
-ipw2200-fw
-irssi
-iwd
-jfsutils
-lftp
-linux
-linux-atm
-linux-firmware
-lsscsi
-lvm2
-man-db
-man-pages
-mc
-mdadm
-mkinitcpio
-mkinitcpio-archiso
-mkinitcpio-nfs-utils
-mtools
-nano
-nbd
-ndisc6
-nfs-utils
-nilfs-utils
-ntfs-3g
-nvme-cli
-openconnect
-reflector
-reiserfsprogs
-rp-pppoe
-rsync
-sdparm
-sg3_utils
-smartmontools
-sudo
-syslinux
-systemd-resolvconf
-tcpdump
-usb_modeswitch
-usbutils
-xfsprogs
-xl2tpd
-bash
+# Allow us to use a standard user account w/ password-less sudo privilege (for building AUR packages later)
+tee -a /etc/sudoers > /dev/null <<EOT
+nobody    ALL=(ALL) NOPASSWD:ALL
 EOT
 
-echo -e "packages.x86_64:\n---"
-echo "$(<${PROFILE}/packages.x86_64)"
+# Install aurutils to build our local repository from AUR packages
+git clone https://aur.archlinux.org/aurutils.git
+chmod 777 aurutils
+cd aurutils
+su -s /bin/sh nobody -c "makepkg -si --noconfirm --noprogressbar" # Make aurutils as a regular user
+cd ../
+
+# Begin setting up our profile
+cp -r /usr/share/archiso/configs/releng/ ${PROFILE}
+cp -rf ./cards/. ${PROFILE}
+mkdir ${LOCAL_REPO}
+repo-add ${LOCAL_REPO}/custom.db.tar.xz
+chmod -R 777 ${LOCAL_REPO}
+sed -i -e "s?~/local-repo?${LOCAL_REPO}?" ${PROFILE}/pacman.conf
+
+# Add packages to our local repository (shared between host and profile)
+cp -f ${PROFILE}/pacman.conf /etc
+mkdir //.cache && chmod 777 //.cache # Since we can't run 'aur sync' as sudo, we have to make the cache directory manually
+#pacman -Rdd granite # We need 'granite-git' (AUR) instead of 'granite' (Community)
+pacman -Rdd gsettings-desktop-schemas
+#su -s /bin/sh nobody -c "aur sync -d custom --root ${LOCAL_REPO} --no-confirm --noview ttf-raleway"
+#su -s /bin/sh nobody -c "aur sync -d custom --root ${LOCAL_REPO} --no-confirm --noview gnome-settings-daemon-elementary"
+#su -s /bin/sh nobody -c "aur sync -d custom --root ${LOCAL_REPO} --no-confirm --noview elementary-wallpapers-git"
+#su -s /bin/sh nobody -c "aur sync -d custom --root ${LOCAL_REPO} --no-confirm --noview pantheon-default-settings"
+#su -s /bin/sh nobody -c "aur sync -d custom --root ${LOCAL_REPO} --no-confirm --noview pantheon-session-git"
+#su -s /bin/sh nobody -c "aur sync -d custom --root ${LOCAL_REPO} --no-confirm --noview switchboard-plug-elementary-tweaks-git"
+#su -s /bin/sh nobody -c "aur sync -d custom --root ${LOCAL_REPO} --no-confirm --noview pantheon-screencast"
+#su -s /bin/sh nobody -c "aur sync -d custom --root ${LOCAL_REPO} --no-confirm --noview pantheon-system-monitor-git"
+#su -s /bin/sh nobody -c "aur sync -d custom --root ${LOCAL_REPO} --no-confirm --noview pantheon-mail-git"
+#su -s /bin/sh nobody -c "aur sync -d custom --root ${LOCAL_REPO} --no-confirm --noview elementary-planner-git"
+su -s /bin/sh nobody -c "aur sync -d custom --root ${LOCAL_REPO} --no-confirm --noview ttf-raleway elementary-wallpapers-git \
+gnome-doc-utils libhandy1 pantheon-default-settings pantheon-session-git switchboard-plug-elementary-tweaks-git pantheon-screencast \
+pantheon-system-monitor-git elementary-planner-git"
+
+echo -e "LOCAL_REPO:\n---"
+ls ${LOCAL_REPO}
 echo "---"
 
-rm -f ${PROFILE}/airootfs/etc/systemd/system/getty@tty1.service.d/autologin.conf
-ln -s /lib/systemd/system/lightdm.service ${PROFILE}/airootfs/etc/systemd/system/display-manager.service
+# Add packages from Arch's repositories to our profile
+tee -a ${PROFILE}/packages.x86_64 > /dev/null <<EOT
+## X11 and drivers
+xorg
+xorg-server
+xorg-drivers
+xorg-xinit
+xorg-xclock
+xorg-twm
+xterm
+xf86-input-libinput
+mesa
+noto-fonts
+ttf-hack
+libva
+libva-mesa-driver
+intel-ucode
+intel-tbb
+## Display & Misc. Desktop Environment
+cups
+cups-pk-helper
+gvfs
+gvfs-afc
+gvfs-mtp
+gvfs-nfs
+gvfs-smb
+light-locker
+lightdm
+simple-scan
+ttf-dejavu
+ttf-droid
+ttf-liberation
+ttf-opensans
+alacritty
+i3-gaps
+## VirtualBox
+virtualbox-guest-utils
+## AUR
+EOT
 
+rm -f ${PROFILE}/airootfs/etc/systemd/system/getty@tty1.service.d/autologin.conf # Remove autologin
+
+# Enable our daemons
+ln -s /lib/systemd/system/lightdm.service ${PROFILE}/airootfs/etc/systemd/system/display-manager.service
+ln -s /lib/systemd/system/NetworkManager.service ${PROFILE}/airootfs/etc/systemd/system/multi-user.target.wants
+ln -s /lib/systemd/system/cups.socket ${PROFILE}/airootfs/etc/systemd/system/sockets.target.wants
+ln -s /lib/systemd/system/avahi-daemon.socket ${PROFILE}/airootfs/etc/systemd/system/sockets.target.wants
+ln -s /lib/systemd/system/bluetooth.service ${PROFILE}/airootfs/etc/systemd/system/bluetooth.target.wants
+
+# Build & bundle the disk image
 mkdir ./out
 mkdir /tmp/archiso-tmp
 mkarchiso -v -w /tmp/archiso-tmp ${PROFILE}
-rm -rf /tmp/archiso-tmp
 mv ./out/cards-*.*.*-x86_64.iso ~
