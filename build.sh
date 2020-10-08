e15fba67597fddb079ba20f4d2f595b6a3102deb
Ty Ethan Sayahi <tysayahi@gmail.com>
2020-10-08 13:59:14 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ umask 0022 # Correct file permissions
 systemd-machine-id-setup # Prevents errors when building AUR packages
 
 pacman -Syu archiso git base-devel jq expac diffstat pacutils wget devtools libxslt cmake \
-intltool mesa gtk-doc gobject-introspection gnome-common polkit dbus-glib go pkg-config libxcb libxcb-util libpam libcairo libglvnd libev libx11 libx11-xcb libxkbcommon>=0.5.0 libxkbcommon-x11>=0.5.0 --noconfirm --noprogressbar # Install packages we'll need to build
+intltool mesa gtk-doc gobject-introspection gnome-common polkit dbus-glib go pkg-config libxcb xcb-util pam cairo libglvnd libev libx11 libxkbcommon>=0.5.0 libxkbcommon-x11>=0.5.0 --noconfirm --noprogressbar # Install packages we'll need to build
 
 # Allow us to use a standard user account w/ password-less sudo privilege (for building AUR packages later)
 tee -a /etc/sudoers > /dev/null <<EOT
@@ -81,13 +81,12 @@ go
 pkg-config
 pkg-config
 libxcb
-libxcb-util
-libpam
-libcairo
+xcb-util
+pam
+cairo
 libglvnd
 libev
 libx11
-libx11-xcb
 libxkbcommon>=0.5.0
 libxkbcommon-x11>=0.5.0
 
