d574f0157d0dec1ee19b499a1db04652ff45063f
Ty Ethan Sayahi <tysayahi@gmail.com>
2020-10-09 13:58:47 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+export HOME=~
+export PROFILE=${HOME}/cards-profile
+export LOCAL_REPO=${HOME}/local-repo
+export ROOT=/__w/cards/cards
+set +h
+umask 0022 # Correct file permissions
+systemd-machine-id-setup # Prevents errors when building AUR packages
+
+pacman -Syu archiso git base-devel jq expac diffstat pacutils wget devtools libxslt cmake \
+intltool mesa gtk-doc gobject-introspection gnome-common polkit dbus-glib go pkg-config libxcb xcb-util pam cairo libglvnd libev ncurses xorg xorg-xauth glibc gcc libx11 libxkbcommon>=0.5.0 libxkbcommon-x11>=0.5.0 --noconfirm --noprogressbar # Install packages we'll need to build
+
+# Allow us to use a standard user account w/ password-less sudo privilege (for building AUR packages later)
+tee -a /etc/sudoers > /dev/null <<EOT
+nobody    ALL=(ALL) NOPASSWD:ALL
+EOT
+
+# Install aurutils to build our local repository from AUR packages
+git clone https://aur.archlinux.org/aurutils.git
+chmod 777 aurutils
+cd aurutils
+su -s /bin/sh nobody -c "makepkg -si --noconfirm --noprogressbar" # Make aurutils as a regular user
+cd ../
