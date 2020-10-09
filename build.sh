ca7597b03ea08e7bd7e11b71d9e3db2f35fb1ba5
Ty Ethan Sayahi <tysayahi@gmail.com>
2020-10-09 12:27:33 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,73 @@
-dir
-ls
-pwd
+#!/bin/bash
+export HOME=~
+export PROFILE=${HOME}/cards-profile
+export LOCAL_REPO=${HOME}/local-repo
+set +h
+umask 0022 # Correct file permissions
+systemd-machine-id-setup # Prevents errors when building AUR packages
+
+pacman -Syu archiso git base-devel jq expac diffstat pacutils wget devtools libxslt cmake \
+intltool mesa gtk-doc gobject-introspection gnome-common polkit dbus-glib go pkg-config libxcb xcb-util pam cairo libglvnd libev ncurses xorg xorg-xauth glibc gcc libx11 libxkbcommon>=0.5.0 libxkbcommon-x11>=0.5.0 --noconfirm --noprogressbar # Install packages we'll need to build
+
+# Allow us to use a standard user account w/ password-less sudo privilege (for building AUR packages later)
+tee -a /etc/sudoers > /dev/null <<EOT
+nobody    ALL=(ALL) NOPASSWD:ALL
+EOT
+
+# Install aurutils to build our local repository from AUR packages
+git clone https://aur.archlinux.org/aurutils.git
+chmod 777 aurutils
+cd aurutils
+su -s /bin/sh nobody -c "makepkg -si --noconfirm --noprogressbar" # Make aurutils as a regular user
+cd ../
+
+# Begin setting up our profile
+cp -r /usr/share/archiso/configs/releng/ ${PROFILE}
+cp -rf ./cards/. ${PROFILE}
+mkdir ${LOCAL_REPO}
+repo-add ${LOCAL_REPO}/custom.db.tar.xz
+chmod -R 777 ${LOCAL_REPO}
+sed -i -e "s?~/local-repo?${LOCAL_REPO}?" ${PROFILE}/pacman.conf
+
+# Add packages to our local repository (shared between host and profile)
+cp -f ${PROFILE}/pacman.conf /etc
+mkdir //.cache && chmod 777 //.cache # Since we can't run 'aur sync' as sudo, we have to make the cache directory manually
+#pacman -Rdd gsettings-desktop-schemas
+su -s /bin/sh nobody -c "aur sync -d custom --root ${LOCAL_REPO} --no-confirm --noview \
+ly \
+yay"
+
+echo -e "LOCAL_REPO:\n---"
+ls ${LOCAL_REPO}
+echo "---"
+
+# Remove the default bloated RELENG
+rm ${PROFILE}/packages.x86_64
+# Add packages from Arch's repositories to our profile
+cat ${HOME}/runner/cards/purged-releng.list >> ${PROFILE}/packages.x86_64
+# Add custom packages to our profile
+cat ${HOME}/runner/cards/user-packages.list >> ${PROFILE}/packages.x86_64
+# Add AUR packages to our profile
+cat ${HOME}/runner/cards/aur-packages.list >> ${PROFILE}/packages.x86_64
+
+rm -f ${PROFILE}/airootfs/etc/systemd/system/getty@tty1.service.d/autologin.conf # Remove autologin
+
+# Enable our daemons
+mkdir -p ${PROFILE}/airootfs/etc/systemd/system/multi-user.target.wants
+mkdir -p ${PROFILE}/airootfs/etc/systemd/system/sockets.target.wants
+mkdir -p ${PROFILE}/airootfs/etc/systemd/system/bluetooth.target.wants
+mkdir -p ${PROFILE}/airootfs/etc/modules-load.d
+
+#ln -s /lib/systemd/system/lightdm.service ${PROFILE}/airootfs/etc/systemd/system/display-manager.service
+ln -s /lib/systemd/system/ly.service ${PROFILE}/airootfs/etc/systemd/system/display-manager.service
+ln -s /lib/systemd/system/NetworkManager.service ${PROFILE}/airootfs/etc/systemd/system/multi-user.target.wants
+ln -s /lib/systemd/system/cups.socket ${PROFILE}/airootfs/etc/systemd/system/sockets.target.wants
+ln -s /lib/systemd/system/avahi-daemon.socket ${PROFILE}/airootfs/etc/systemd/system/sockets.target.wants
+ln -s /lib/systemd/system/bluetooth.service ${PROFILE}/airootfs/etc/systemd/system/bluetooth.target.wants
+#ln -s /lib/modules-load.d/virtualbox-guest-dkms.conf ${PROFILE}/airootfs/etc/modules-load.d
+
+# Build & bundle the disk image
+mkdir ./out
+mkdir /tmp/archiso-tmp
+mkarchiso -v -w /tmp/archiso-tmp ${PROFILE}
+mv ./out/cards-*.*.*-x86_64.iso ~
