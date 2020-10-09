b9adb199b5a4dbe99b0e444ba3ef70b2e1450859
Ty Ethan Sayahi <tysayahi@gmail.com>
2020-10-09 12:38:25 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
           echo "GITHUB_WORKSPACE = $GITHUB_WORKSPACE"
-          echo "github.workspace = ${{ github.workspace }}"
+          echo "github.workspace = ${{github.workspace}}"
           echo "pr.ref = ${{github.event.pull_request.head.ref}}"
-          echo "github.ref = ${{ github.ref }}"
+          echo "github.ref = ${{github.ref}}"
           echo "$GITHUB_CONTEXT"
            echo $GITHUB_WORKSPACE && pwd && echo ${{github.ref}}
