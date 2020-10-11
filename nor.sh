2c22c0634891ae5206c55044bd29402b4f0b19db
Eile <tysayahi@gmail.com>
2020-10-10 21:15:37 -0400

    export LC_ALL=en_US.UTF-8

--- nor.sh
+++ nor.sh
@@ -3,6 +3,7 @@ export HOME=~
 export PROFILE=${HOME}/cards-profile
 export LOCAL_REPO=${HOME}/local-repo
 export ROOT=/__w/cards/cards
+export LC_ALL=en_US.UTF-8
 set +h
 umask 0022 # Correct file permissions
 systemd-machine-id-setup # Prevents errors when building AUR packages
